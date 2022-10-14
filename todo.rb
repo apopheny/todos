7758a6160d3aedcabd3a9605b28966510bce21c8
Chris Douglass <cedouglass@gmail.com>
2022-10-14 06:45:07 -0400

    improved list name editing functionality

--- todo.rb
+++ todo.rb
@@ -5,13 +5,22 @@ require 'sinatra/reloader'
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
-def valid_list_name?(new_name)
+def valid_list_name?(new_name, update: false)
+  new_name = new_name.strip
   if !(1..100).cover?(new_name.size)
     session[:error] = 'List name must be between 1 and 100 characters.'
-  elsif session[:lists].any? { |list| list[:name].downcase == new_name.downcase }
-    session[:error] = 'List name already exists.'
+  elsif session[:lists].any? { |list| list[:name] == new_name }
+    handle_duplicates(new_name, update)
   else
-    session[:success] = 'The list has been created.'
+    session[:success] = update ? 'The list has been updated.' : 'The list has been created.'
+  end
+end
+
+def handle_duplicates(name, update)
+  if update && session[:lists].none? { |list| list[:name] == name }
+    session[:success] = 'List name saved'
+  else
+    session[:error] = 'List name already exists.'
   end
 end
 
@@ -54,20 +63,19 @@ get '/lists/:id' do
   erb :todo_items
 end
 
-get '/lists/edit/:id' do
-  @id = params[:id].to_i
+get '/lists/:id/edit' do
   erb :edit_list
 end
 
-post '/lists/edit/:id' do
-  list_name = params[:list_name].strip
-  valid_list_name?(list_name)
+post '/lists/:id/edit' do
   list_id = params[:id].to_i
+  list_name = params[:list_name]
+  valid_list_name?(list_name, update: true)
 
   if session[:error]
-    redirect '/lists/edit/:id'
+    erb :edit_list
   elsif session[:success]
     session[:lists][list_id][:name] = list_name
-    redirect '/lists'
+    redirect "/lists/#{list_id}"
   end
 end
